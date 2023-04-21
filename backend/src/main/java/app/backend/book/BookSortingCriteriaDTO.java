79c88ba9f4e6a13d68bae18f0b09d8879b3963da
gabriela <gab563riela@gmail.com>
2023-04-21 12:44:18 +0200

    Added validation to BookSortingCriteriaDTO.

--- backend/src/main/java/app/backend/book/BookSortingCriteriaDTO.java
+++ backend/src/main/java/app/backend/book/BookSortingCriteriaDTO.java
@@ -1,11 +1,19 @@
 package app.backend.book;
 
+import app.backend.utils.annotations.ValidYear;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class BookSortingCriteriaDTO {
+
+    @ValidYear
     private final Integer minPublishYear;
+
+    @ValidYear
     private final Integer maxPublishYear;
+
+    @NotBlank
     private final String typeName;
 
     public BookSortingCriteriaDTO(String type, Integer min, Integer max) {
