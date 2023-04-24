14ad3b3dcb4fbf67e26ca2d31369c21a5c06beb9
gabriela <gab563riela@gmail.com>
2023-04-24 16:11:37 +0200

    Updated UserMapper.

--- backend/src/main/java/app/backend/user/UserMapper.java
+++ backend/src/main/java/app/backend/user/UserMapper.java
@@ -1,26 +1,30 @@
 package app.backend.user;
 
+import app.backend.book.BookEntity;
 import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.ERROR)
+        unmappedTargetPolicy = ReportingPolicy.ERROR,
+        imports = BookEntity.class)
 public interface UserMapper {
 
     UserDTO toDTO(UserEntity user);
 
     List<UserDTO> toDTOs(List<UserEntity> userEntities);
 
+    @Mapping(target = "password", ignore = true)
     @Mapping(target = "roles", expression = "java(new ArrayList<RoleEntity>())")
-    @Mapping(target = "books", expression = "java(new ArrayList<RoleEntity>())")
+    @Mapping(target = "books", expression = "java(new ArrayList<BookEntity>())")
     UserEntity toEntity(UserDTO user);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "roles", expression = "java(new ArrayList<RoleEntity>())")
-    @Mapping(target = "books", expression = "java(new ArrayList<RoleEntity>())")
+    @Mapping(target = "books", expression = "java(new ArrayList<BookEntity>())")
     UserEntity toEntity(RegisteredUserDTO user);
 
+    @Mapping(target = "password", ignore = true)
     @Mapping(target = "roles", ignore = true)
     @Mapping(target = "books", ignore = true)
     void updateEntity(@MappingTarget UserEntity userToUpdate, UserDTO updatedUser);
