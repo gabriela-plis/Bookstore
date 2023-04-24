a76add8018b73f7b078b0ed3ed3cc69eadfb46f3
gabriela <gab563riela@gmail.com>
2023-04-24 16:03:49 +0200

    Added Cors config.

--- backend/src/main/java/app/backend/security/SecurityConfig.java
+++ backend/src/main/java/app/backend/security/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.context.DelegatingSecurityContextRepository;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -38,6 +43,17 @@ public class SecurityConfig {
             .build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     // 5
     @Bean
@@ -50,7 +66,7 @@ public class SecurityConfig {
 
         // 1
         http.csrf().disable()
-            .cors().disable();
+            .cors();
 
         http.authorizeHttpRequests()
             .requestMatchers("/users/register").permitAll()
@@ -70,6 +86,7 @@ public class SecurityConfig {
             .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
         // 2
+
         http.formLogin(withDefaults());
 
         return http.build();
