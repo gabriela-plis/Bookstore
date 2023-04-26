7c08142e7f30056d43735cd424fd36acef56ad18
gabriela <gab563riela@gmail.com>
2023-04-26 16:21:16 +0200

    BCryptPasswordEncoder changes.

--- backend/src/main/java/app/backend/security/SecurityConfig.java
+++ backend/src/main/java/app/backend/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -55,9 +54,8 @@ public class SecurityConfig {
         return source;
     }
 
-    // 5
     @Bean
-    public PasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
