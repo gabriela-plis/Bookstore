0e09b040fe0ec10beb2460bb73734063451485bd
gabriela <gab563riela@gmail.com>
2023-05-02 18:30:06 +0200

    Borrows - added return book logic.

--- frontend/src/components/user-page/Borrows.tsx
+++ frontend/src/components/user-page/Borrows.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import OperationTypes from "../../OperationTypes";
 import BookList, { Operation } from "../../reusable-components/BookList";
 
 const Borrows = () => {
 
     const [bookId, setBookId] = useState(0);
+   
+    const [trigger, forceUpdate] = useReducer(x => x + 1, 0);
 
     const handleReturn = () => {
+        fetch(`http://localhost:8080/books/${bookId}/return`, {
+            method: "POST",
+            credentials: "include"
+        }).then( () => {
+            forceUpdate();
+        })
 
     }
 
@@ -18,7 +26,7 @@ const Borrows = () => {
     return (
         <section className="borrows">
             <h2 className="borrows__title">My Borrows</h2>
-            <BookList url={'http://localhost:8080/books/user/' + sessionStorage.getItem("email")} operation={operation} bookId={bookId} setBookId={setBookId} />
+            <BookList url={"http://localhost:8080/books/user/" + sessionStorage.getItem("id")} operation={operation} bookId={bookId} setBookId={setBookId} forceUpdate={trigger}/>
         </section>
      );
 }
