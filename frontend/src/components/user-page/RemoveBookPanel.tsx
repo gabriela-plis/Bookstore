88b91beaca3cc42d464cc4ee88be2156e958e099
gabriela <gab563riela@gmail.com>
2023-05-02 18:26:01 +0200

    RemoveBookPanel - page re-render after operation.

--- frontend/src/components/user-page/RemoveBookPanel.tsx
+++ frontend/src/components/user-page/RemoveBookPanel.tsx
@@ -1,19 +1,19 @@
 import OperationTypes from "../../OperationTypes";
-import Book from "../../DTO/BookDTO";
 import BookList, { Operation } from "../../reusable-components/BookList";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 const RemoveBookPanel = () => {
 
     const [bookId, setBookId] = useState(0);
+    const [trigger, forceUpdate] = useReducer(x => x + 1, 0);
 
     const handleRemove = () => {
         fetch('http://localhost:8080/books/' + bookId, {
             method: "DELETE",
             credentials: "include"
+        }).then( () => {
+            forceUpdate();
         })
-
-        //feedback
     }
 
     const operation: Operation = {
@@ -24,7 +24,7 @@ const RemoveBookPanel = () => {
     return ( 
         <section className="remove-panel">
             <h2 className="remove-panel__title">Remove Book</h2>
-            <BookList url="http://localhost:8080/books/to-remove" operation={operation} bookId={bookId} setBookId={setBookId}/>
+            <BookList url="http://localhost:8080/books/to-remove" operation={operation} bookId={bookId} setBookId={setBookId} forceUpdate={trigger}/>
         </section>
      );
 }
